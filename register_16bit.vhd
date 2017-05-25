0e239c324d0be2ad895acb907f6635bcb88e31d5
Anthony Byrne <abyrne19@bu.edu>
2017-05-25 11:30:37 +1000

    Added PC and fixed formatting

--- register_16bit.vhd
+++ register_16bit.vhd
@@ -3,24 +3,24 @@ USE ieee.std_logic_1164.all;
 -- 16-bit Register w/ Async. Reset 
 ENTITY register_16bit IS 
 	PORT(
-		input: in STD_LOGIC_VECTOR(16 DOWNTO 0);
+		input: in STD_LOGIC_VECTOR(16 downto 0);
 		enable: in STD_LOGIC;
 		reset: in STD_LOGIC; -- async. reset
 		clock: in STD_LOGIC;
-		output: out STD_LOGIC_VECTOR(16 DOWNTO 0)
+		output: out STD_LOGIC_VECTOR(16 downto 0)
 	);
 END register_16bit;
 
 ARCHITECTURE behavioural OF register_16bit IS
 BEGIN
-    PROCESS(clock, reset)
-    BEGIN
-        IF (reset = '1') THEN
-            output <= x"00000000";
-        ELSIF rising_edge(clock) THEN
-            IF enable = '1' THEN
-                output <= input;
-            END IF;
-        END IF;
-    END PROCESS;
+	PROCESS(clock, reset)
+	BEGIN
+		IF (reset = '1') THEN
+			output <= x"00000000";
+		ELSIF rising_edge(clock) THEN
+			IF enable = '1' THEN
+				output <= input;
+			END IF;
+		END IF;
+	END PROCESS;
 END behavioural;
